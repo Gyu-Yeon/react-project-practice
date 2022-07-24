aeb586b594cbe5c71d34ccd9c0ed246dad8187f7
Gyu-Yeon <idsopy@nate.com>
2022-07-24 23:48:53 +0900

    1

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import TodoList from './TodoList';
 import TodoTemplate from './TodoTemplate';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리엑트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked: false,
-    },
-  ]);
+  function createBulkTodos() {
+    const array = [];
+    for (let i = 0; i <= 2500; i++) {
+      array.push({
+        id: i,
+        text: `할일 ${i}`,
+        checked: false,
+      });
+    }
+    return array;
+  }
+
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
 
@@ -37,9 +33,12 @@ function App() {
     [todos],
   );
 
-  const onClickRemove = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  const onClickRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
 
   const onToggle = (id) => {
     setTodos(
